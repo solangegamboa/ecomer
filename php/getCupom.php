6731e331d22fb118e6586b368fbf88f0f931c6e2
solsgamboa <solange.gamboa@dentalspeed.com>
2022-03-04 18:03:53 -0300

    cupom funcional

--- php/getCupom.php
+++ php/getCupom.php
@@ -3,7 +3,7 @@
 if ($_POST) {
     if ($_POST['cpf']) {
         $user_arr = [];
-        $mysqli = new mysqli('127.0.0.1', 'root', 'Dm$M20@zBx50ZN', 'ecomer');
+        $mysqli = new mysqli('127.0.0.1', 'phpuser', 'password@123', 'ecomer');
         if ($mysqli->connect_errno) {
             echo "Failed to connect to MySQL: " . $mysqli->connect_error;
             exit();
@@ -12,7 +12,7 @@ if ($_POST) {
         // Perform query
         if ($result = $mysqli->query("SELECT * FROM cupom WHERE cpf = '" . $_POST['cpf'] . "' AND resgatado = 0")) {
             if ($result->num_rows <= 0) {
-                echo json_encode(['message' => 'Desculpe não encontramos o seu nome na nossa lista, Quem sabe da próxima vez.', 'status' => 'error']);
+                echo json_encode(['message' => 'Desculpe não encontramos um cupom válido em seu nome, Quem sabe da próxima vez.', 'status' => 'error']);
                 exit();
             }
             
@@ -23,7 +23,6 @@ if ($_POST) {
             if (!empty($fetch)) {
                 echo json_encode(['message' => 'Seu cupom: <strong>' . $fetch[1] . '</strong>', 'status' => 'sucesso']);
                 $sql = "UPDATE cupom SET resgatado = '1', dt_resgate = NOW() WHERE id = '" . $fetch[0] . "'";
-                echo $sql;
                 if (!mysqli_query($mysqli, $sql)) {
                     echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
                 }
