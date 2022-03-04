6b1dc31abc83a86bc6cdf3cf6feb6875c93b13b3
solsgamboa <solange.gamboa@dentalspeed.com>
2022-03-04 17:48:06 -0300

    cupom funcional

--- js/main.js
+++ js/main.js
@@ -9,41 +9,30 @@
         if ($('#contactForm').length > 0) {
             $('#contactForm').validate({
                 rules: {
-                    name: 'required',
-                    subject: 'required',
-                    email: {
-                        required: true,
-                        email: true
-                    },
-                    message: {
-                        required: true,
-                        minlength: 5
+                    name: 'required', subject: 'required', email: {
+                        required: true, email: true
+                    }, message: {
+                        required: true, minlength: 5
                     }
-                },
-                messages: {
+                }, messages: {
                     name: 'Please enter your name',
                     subject: 'Please enter your subject',
                     email: 'Please enter a valid email address',
                     message: 'Please enter a message'
-                },
-                /* submit via ajax */
+                }, /* submit via ajax */
 
                 submitHandler: function (form) {
-                    var $submit = $('.submitting'),
-                        waitText = 'Resgatando....';
+                    var $submit = $('.submitting'), waitText = 'Resgatando....';
 
                     $.ajax({
-                        type: 'POST',
-                        url: 'php/getCupom.php',
-                        data: $(form).serialize(),
+                        type: 'POST', url: 'php/getCupom.php', data: $(form).serialize(),
 
                         beforeSend: function () {
                             $submit.css('display', 'block').text(waitText);
-                        },
-                        success: function (msg) {
+                        }, success: function (msg) {
                             var mjson = JSON.parse(msg);
-                            if (mjson.status === 'success') {
-                                $('#form-message-success').html(mjson.mensagem);
+                            if (mjson.status === 'sucesso') {
+                                $('#form-message-success').html(mjson.message);
                                 $('#form-message-warning').hide();
                                 setTimeout(function () {
                                     $('#contactForm').fadeIn();
@@ -64,12 +53,11 @@
                                 }, 1400);
 
                             } else {
-                                $('#form-message-warning').html(mjson.mensagem);
+                                $('#form-message-warning').html(mjson.message);
                                 $('#form-message-warning').fadeIn();
                                 $submit.css('display', 'none');
                             }
-                        },
-                        error: function () {
+                        }, error: function () {
                             $('#form-message-warning').html('Something went wrong. Please try again.');
                             $('#form-message-warning').fadeIn();
                             $submit.css('display', 'none');
@@ -81,7 +69,6 @@
         }
     };
     contactForm();
-
 
 
 })(jQuery);
